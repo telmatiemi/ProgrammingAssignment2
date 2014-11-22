46dd4290507dfb862ad7a667388feef28229aeef
Telma <telma_tiemi@hotmail.com>
2014-11-21 23:59:23 -0200

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below return cached values of an input matrix and its inverse matrix.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix reads and keep the value of the input matrix x.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  
+  set <- function(y) {
+    
+    x <<- y
+    m <<- NULL
+    
+  }
+  
+  get <- function() { x }
+  
+  setinv <- function(solve) {m <<- solve }
+  
+  getinv <- function() { m }
+  
+  list(set = set, 
+       get = get,
+       setinv = setinv,
+       getinv = getinv )
+  
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+        
+  m <- x$getinv()               # accesses the object 'x' and gets the value of the inverse
+  
+  if(!is.null(m)) {              # if mean was already cached (not NULL)
+    
+    message("getting cached data")
+    
+    return(m)                       # return the inverse
+    #   the function cachemean(), note
+  }
+  
+  data <- x$get()        
+  
+  m <- solve(data)   # if m was NULL then we have to calculate the inverse
+  
+  x$setinv(m)         
+  
+  m                      
+  
 }
